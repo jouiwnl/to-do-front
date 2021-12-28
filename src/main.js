413765a727e02df119e6f38c5bc40e7d652833b4
jouiwnl <jou.098olo@gmail.com>
2021-12-27 23:06:00 -0300

    teste push

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import './styles'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
 export const eventBus = new Vue();
 
 new Vue({
