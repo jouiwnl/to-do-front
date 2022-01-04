182c817fc6a442a9853b4ed3d5b86d2dabd75cdc
jouiwnl <jou.098olo@gmail.com>
2022-01-04 18:17:15 -0300

    change http

--- src/components/services/config.js
+++ src/components/services/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const http = axios.create({
-    baseURL: 'https://ec2-18-230-113-105.sa-east-1.compute.amazonaws.com:443/'
+    baseURL: 'ec2-18-230-113-105.sa-east-1.compute.amazonaws.com:443/'
     //baseURL: 'http://localhost:3335/'
 });
