45f6425505a1cea5aaf0e13ce0ef3b2e2b75b952
jouiwnl <jou.098olo@gmail.com>
2022-01-04 18:00:28 -0300

    troca api para um api brasileira com menos delay

--- src/components/services/config.js
+++ src/components/services/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const http = axios.create({
-    baseURL: 'ec2-18-230-113-105.sa-east-1.compute.amazonaws.com:3335/'
+    baseURL: 'http://ec2-18-230-113-105.sa-east-1.compute.amazonaws.com:3335/'
     //baseURL: 'http://localhost:3335/'
 });
